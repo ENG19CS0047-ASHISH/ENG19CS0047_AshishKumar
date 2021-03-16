9def8a40e1f37aa326e2d7ed905cfa045181f1b6
ENG19CS0047-ASHISH <76139721+ENG19CS0047-ASHISH@users.noreply.github.com>
2021-03-16 12:38:28 +0530

    Update tromboloid_with_4_functions.c

--- tromboloid_with_4_functions.c
+++ tromboloid_with_4_functions.c
@@ -1,1 +1,29 @@
-//WAP to find the volume of a tromboloid using 4 functions.
+#include<stdio.h>
+float inp1(){
+float x;
+printf("Enter value of h ");
+scanf("%f",&x);
+return x;
+}
+float inp2(){
+    float y;
+printf("Enter value of b");
+scanf("%f",&y);
+return y;
+}
+float inp3(){
+    float z;
+printf("Enter value of d");
+scanf("%f",&z);
+return  z;
+}
+float out(float x,float y,float z)
+{
+return 0.333*(x*y*z)+(y/z);}
+int main(){
+float a=inp1();
+float b=inp2();
+float c=inp3();
+float ans=out(a,b,c);
+printf("Volume is %f",ans);
+return 0;}
