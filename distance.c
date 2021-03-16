b109909d35cbd73fd3f36756c0ac4578ab02d1ac
ENG19CS0047-ASHISH <76139721+ENG19CS0047-ASHISH@users.noreply.github.com>
2021-03-16 13:46:07 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,1 +1,28 @@
-//WAP to find the distance between two point using 4 functions.
+#include<stdio.h>
+#include<math.h>
+int difordinatesinp()
+{
+int x2;
+int x1;
+printf("Enter ordinates");
+scanf("%d %d",&x2,&x1);
+return x2-x1;}
+int difabsiscainp()
+{
+int y2,y1;
+printf("Enter the absisca");
+scanf("%d %d",&y2,&y1);
+return y2-y1;}
+int dist(int o,int a){
+return (o)*(o)+(a)*(a);
+}
+double finaldist(int k){
+return sqrt(k);}
+int main(){
+int i=difordinatesinp();
+int j=difabsiscainp();
+int m=dist(i,j);
+double ans=finaldist(m);
+printf("Distance is %lf",ans);
+
+}
